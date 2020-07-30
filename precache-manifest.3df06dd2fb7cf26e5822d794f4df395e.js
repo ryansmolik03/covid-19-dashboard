a1d351d68d94275d103e1e81a0cfa18494d3022b
Ryan Smolik <ryansmolik@protonmail.com>
2020-07-29 22:55:54 -0400

    Updates

---
+++ precache-manifest.3df06dd2fb7cf26e5822d794f4df395e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7bca687ebea84283c99725c492b4f2",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "7ea96407235395c6ee7d",
+    "url": "/covid-19-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "95371c89cd290ac8caf9",
+    "url": "/covid-19-dashboard/static/css/main.d1e263e3.chunk.css"
+  },
+  {
+    "revision": "7ea96407235395c6ee7d",
+    "url": "/covid-19-dashboard/static/js/2.443a8340.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19-dashboard/static/js/2.443a8340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95371c89cd290ac8caf9",
+    "url": "/covid-19-dashboard/static/js/main.a006998f.chunk.js"
+  },
+  {
+    "revision": "0d5878ec4bbcb5c67f1a",
+    "url": "/covid-19-dashboard/static/js/runtime-main.8d77b089.js"
+  }
+]);
