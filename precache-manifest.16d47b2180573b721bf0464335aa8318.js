ddc4d7da054e472f84cbe38a550d713f39577937
Ryan Smolik <ryansmolik@protonmail.com>
2020-08-01 14:10:09 -0400

    Updates

---
+++ precache-manifest.16d47b2180573b721bf0464335aa8318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75241c1d6fa0d30e3bd3e2322ea69d76",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "db93cc90eb28d3a04aa7",
+    "url": "/covid-19-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c2a3e6410cbea3070bc",
+    "url": "/covid-19-dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db93cc90eb28d3a04aa7",
+    "url": "/covid-19-dashboard/static/js/2.0a80e97a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19-dashboard/static/js/2.0a80e97a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2a3e6410cbea3070bc",
+    "url": "/covid-19-dashboard/static/js/main.2c42e95f.chunk.js"
+  },
+  {
+    "revision": "0d5878ec4bbcb5c67f1a",
+    "url": "/covid-19-dashboard/static/js/runtime-main.8d77b089.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-dashboard/precache-manifest.bdaf197bfe68d19969e13273b32e6b3f.js"
+  "/covid-19-dashboard/precache-manifest.16d47b2180573b721bf0464335aa8318.js"
 );
 
 self.addEventListener('message', (event) => {
